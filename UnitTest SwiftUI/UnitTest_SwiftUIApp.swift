43ae9e984661d4ea1f0516a5c8e87b4bb3c28da2
shahriarRahmanShaon <shaon463@gmail.com>
2022-02-26 02:42:13 +0600

    Initial Commit

---
+++ UnitTest SwiftUI/UnitTest_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  UnitTest_SwiftUIApp.swift
+//  UnitTest SwiftUI
+//
+//  Created by sergio shaon on 26/2/22.
+//
+
+import SwiftUI
+
+@main
+struct UnitTest_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
