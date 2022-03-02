35ac0bbf030ab8913c860dc14cc247d3021ea7ff
shahriarRahmanShaon <shaon463@gmail.com>
2022-03-02 17:28:14 +0600

    Simple math test

---
+++ UnitTest SwiftUI/SimpleMath.swift
@@ -0,0 +1,17 @@
+//
+//  SimpleMath.swift
+//  UnitTest SwiftUI
+//
+//  Created by sergio shaon on 2/3/22.
+//
+
+import Foundation
+
+class SimpleMath {
+    func addTen(number: Int) -> Int{
+       return number + 10
+    }
+    func beSquare(number: Int) -> Int{
+        return number * number
+    }
+}
